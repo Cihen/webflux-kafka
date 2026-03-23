bdb0fcbf3d5600ecdeccc173da5c0aa16316ba4d
Chien <thucchien02@gmail.com>
2026-03-23 22:58:46 +0700

    part 3: init liquibase scripts for profile service

---
+++ profileservice/src/main/resources/db/scripts/sql/table-profile-init.sql
@@ -0,0 +1,8 @@
+INSERT INTO profile (id, email, status, name, role)
+VALUES (1, 'dev01@gmail.com', 'ACTIVE', 'Nguyễn Thúc Chiến', 'DEVELOPER'),
+VALUES (2, 'dev02@gmail.com', 'ACTIVE', 'Nguyễn Thị Thanh Quỳnh', 'DEVELOPER'),
+VALUES (3, 'dev03@gmail.com', 'ACTIVE', 'Nguyễn Thúc Lợi', 'DEVELOPER'),
+VALUES (4, 'customer01@gmail.com', 'ACTIVE', 'Nguyễn Kiêm Bắc', 'CUSTOMER'),
+VALUES (5, 'customer02@gmail.com', 'ACTIVE', 'Cao Duy Dũng', 'CUSTOMER'),
+VALUES (6, 'customer03@gmail.com', 'ACTIVE', 'Cao Minh Vũ', 'CUSTOMER'),
+VALUES (7, 'customer04@gmail.com', 'ACTIVE', 'Vũ Đức Duy', 'CUSTOMER');
